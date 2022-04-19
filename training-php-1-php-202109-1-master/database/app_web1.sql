e5a83e72a68104e82b90502c201532ceef0ceb06
AnPhan <tlt2542001@gmail.com>
2022-04-19 21:27:01 +0700

    Show user

--- training-php-1-php-202109-1-master/database/app_web1.sql
+++ training-php-1-php-202109-1-master/database/app_web1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1:3306
--- Thời gian đã tạo: Th4 17, 2022 lúc 12:55 PM
+-- Thời gian đã tạo: Th4 19, 2022 lúc 02:05 PM
 -- Phiên bản máy phục vụ: 5.7.36
 -- Phiên bản PHP: 7.4.26
 
@@ -24,31 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `banks`
---
-
-DROP TABLE IF EXISTS `banks`;
-CREATE TABLE IF NOT EXISTS `banks` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `cost` float NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `banks`
---
-
-INSERT INTO `banks` (`id`, `user_id`, `cost`) VALUES
-(1, 1, 1111),
-(2, 2, 1111),
-(3, 3, 1111),
-(4, 4, 1111),
-(5, 5, 1111);
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `users`
 --
 
@@ -56,23 +31,24 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
-  `fullname` varchar(100) NOT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
   `email` varchar(100) NOT NULL,
-  `phone` varchar(10) NOT NULL,
-  `sex` varchar(20) NOT NULL,
-  `lastname` varchar(50) NOT NULL,
-  `type` varchar(100) NOT NULL,
-  `password` varchar(250) NOT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `sex` varchar(20) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `password` varchar(250) DEFAULT NULL,
   `cost` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Đang đổ dữ liệu cho bảng `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `fullname`, `email`, `phone`, `sex`, `lastname`, `type`, `password`, `cost`) VALUES
-(3, 'hackerasfasf', '', '', '', '', '', '', '6a204bd89f3c8348afd5c77c717a097a', NULL);
+(3, 'hackerasfasf', '', '', '', '', '', '', '6a204bd89f3c8348afd5c77c717a097a', NULL),
+(5, 'an1234', NULL, 'an1234@gmail.com', NULL, NULL, NULL, NULL, '81dc9bdb52d04dc20036dbd8313ed055', NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
